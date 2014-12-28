f1789b9343a291ef1803fafe3a3da2bfb27a47c5
AnubhabB <anubhab@quicklyapp.co>
2014-12-28 13:46:01 +0530

    Update sketch with zoom

--- application/static/js/sketch.js
+++ application/static/js/sketch.js
@@ -137,32 +137,22 @@ function sketch(action){
   sketch.prototype.newLayerRedraw = function(newX0,newY0,newX1,newY1) {
 
     var self = this;
-    //DRAW A FULL CANVAS, forget zoom n shit for now;
+    
     var calcL = newX0 + globalLeft;
     var calcT = newY0 + globalTop;
     var calcW = newX1 - newX0;
     var calcH = newY1 - newY0;
 
 
-    $(".containerMain").append("<canvas id='t_cnv' style='position:fixed;z-index:1000;left:"+calcL+"px;top:"+calcT+"px;width:"+calcW+"px;height:"+calcH+"px;border:1px solid #ff0' width="+calcW+" height="+calcH+"></canvas>");
-    /*var calcL = globalLeft + newX0;
-    var calcT = globalTop + newY0;
-    var calcW = newX1 - newX0;
-    var calcH = newY1 - newY0;
+    $(".containerMain").append("<canvas id='t_cnv' style='position:fixed;z-index:1000;left:"+calcL+"px;top:"+calcT+"px;width:"+calcW+"px;height:"+calcH+"px;' width="+calcW+" height="+calcH+"></canvas>");
 
-    $(".containerMain").append("<canvas id='t_cnv' style='position:fixed;z-index:1000;left:"+calcL+"px;top:"+calcT+"px;width:"+calcW+"px;height:"+calcH+"px;border:1px solid #ff0' width="+calcW+" height="+calcH+"></canvas>");
-
-    alert("Yellow");
-    //GET ALL VALUES OF CURRENT CANVAS - image, z-index and draw it on to t_cnv
     var cv = document.getElementById("Canvas"+currentIndex);
     var z  = $("#Canvas"+currentIndex).css("z-index");
     var ctx = document.getElementById("t_cnv").getContext("2d");
 
-    //GET TOP LEFT for draw
-    var t_left= parseInt($("#Canvas"+currentIndex).offset().left) - parseInt($("#t_cnv").offset().left);
-    var t_top = parseInt($("#Canvas"+currentIndex).offset().top) -  parseInt($("#t_cnv").offset().top);
-    alert("Post Yellow");*/
-    /*
+    var t_left = imageLayers[currentIndex].left + globalLeft -  parseInt($("#t_cnv").offset().left);
+    var t_top  = imageLayers[currentIndex].top + globalTop -  parseInt($("#t_cnv").offset().top);
+
     ctx.drawImage(cv,t_left,t_top);
 
     t_left= parseInt($("#temp_canvas").offset().left) - parseInt($("#t_cnv").offset().left);
@@ -172,7 +162,16 @@ function sketch(action){
 
     $("#Canvas"+currentIndex).remove();
     $("#temp_canvas").remove();
-    $("#t_cnv").css("z-index",z).addClass("canvasClass").attr("id","Canvas"+currentIndex);*/
+
+    $("#t_cnv").css("z-index",z).addClass("canvasClass").attr("id","Canvas"+currentIndex);
+    $("#Canvas"+currentIndex).css({
+      "left"  : calcL * zoom.zoomfactor +"px",
+      "top"   : calcT * zoom.zoomfactor +"px",
+      "width" : calcW * zoom.zoomfactor +"px",
+      "height": calcH * zoom.zoomfactor +"px"
+    });
+    imageLayers[currentIndex].width  = calcW;
+    imageLayers[currentIndex].height = calcH;
   };
 
   sketch.prototype.updateBrushSize = function() {
